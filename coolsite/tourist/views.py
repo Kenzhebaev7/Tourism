f37db34cc4166855185e9cbc3e4f0e59c7e7383a
Kenzhebaev7 <124691711+Kenzhebaev7@users.noreply.github.com>
2023-04-27 23:42:06 +0600

    Final

--- coolsite/tourist/views.py
+++ coolsite/tourist/views.py
@@ -1,27 +1,157 @@
-from django.http import HttpResponse,HttpResponseNotFound,Http404
-from django.shortcuts import render, redirect
+from rest_framework import generics
+from django.contrib.auth import logout, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.core.paginator import Paginator
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from .forms import *
 from .models import *
+from .serializers import TouristSerializer
+from .utils import *
 
-menu=["Сайт туралы", "Қосу", "Кері байланыс", "Кіру"]
-def index(request):
-    posts=Tourist.objects.all()
-    return render(request, 'tourist/index.html', {'posts': posts, 'menu': menu, 'title': 'Басты бет'})
+
+
+class TouristAPIView(APIView):
+    def get(self, request):
+        lst=Tourist.objects.all().values()
+        return Response({'posts':list(list)})
+
+    def post(self, request):
+        post_new = Tourist.objects.create(
+            title=request.data('title')
+        )
+        return Response({'title': 'Medeu'})
+
+
+class TouristHome(DataMixin, ListView):
+    model = Tourist
+    template_name = 'tourist/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Главная страница")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        return Tourist.objects.filter(is_published=True).select_related('cat')
+
 
 def about(request):
-    return render(request, 'tourist/about.html', {'menu': menu, 'title': 'Сайт туралы'})
+    contact_list = Tourist.objects.all()
+    paginator = Paginator(contact_list, 3)
 
-def categories(request, catid):
-    if request.POST:
-        print(request.POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'tourist/about.html', {'page_obj': page_obj, 'menu': menu, 'title': 'О сайте'})
 
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
-def archive(request,year):
-    if int(year)>2020:
-        return redirect('home', permanent=True)
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'tourist/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True
 
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Добавление статьи")
+        return dict(list(context.items()) + list(c_def.items()))
 
-def pageNotFound(request,exception):
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'tourist/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Обратная связь")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
+
+
+# def login(request):
+#     return HttpResponse("Авторизация")
+
+
+def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+class ShowPost(DataMixin, DetailView):
+    model = Tourist
+    template_name = 'tourist/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class TouristCategory(DataMixin, ListView):
+    model = Tourist
+    template_name = 'tourist/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Tourist.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Категория - ' + str(c.name),
+                                      cat_selected=c.pk)
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'tourist/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'tourist/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+class TouristAPIView(generics.ListAPIView):
+    queryset = Tourist.objects.all()
+    serializer_class = TouristSerializer
